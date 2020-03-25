2b73516d120ca390a4eb4a823ee9a931436eb3a5
reisdev <matheusdrdj@gmail.com>
2020-03-25 09:54:49 -0300

    Fix thumbnail image

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -5,12 +5,6 @@ export default (bot: Client, msg: Message) => {
     switch (msg.content.trim()) {
         case `${process.env.PREFIX}since`:
             const since = moment(msg.member.joinedTimestamp).format("DD/MM/YYYY");
-            /*const embed = new Discord.MessageEmbed()
-                .setAuthor(msg.guild.name, `https://cdn.discordapp.com/icons/${msg.guild.id}/${msg.guild.icon}.png`, process.env.SERVER_URL).setColor(3447003)
-                .setTitle(msg.member.nickname || msg.author.username)
-                .setThumbnail(`https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.png`)
-                .setDescription(`Congratulations! You are a member of this server since ${since}.`)
-                .setTimestamp()*/
             msg.channel.send({
                 embed: {
                     author: {
@@ -18,7 +12,9 @@ export default (bot: Client, msg: Message) => {
                         icon_url: `https://cdn.discordapp.com/icons/${msg.guild.id}/${msg.guild.icon}.png`
                     },
                     title: msg.member.nickname || msg.author.username,
-                    thumbnail: `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.png`,
+                    thumbnail: {
+                        url: `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.png`
+                    },
                     description: `Congratulations! You are a member of this server since ${since}.`,
                     timestamp: true
                 }
